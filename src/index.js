e6467b4aaf2a1d9d8d9d0c5eefa982c09e40d242
Matt <matthewsharp123@gmail.com>
2016-10-06 21:52:58 +0100

    add canvas

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const Hapi = require('hapi');
+const path = require('path');
+
+const server = new Hapi.Server();
+server.connection({ port: 8080 });
+
+
+
+server.register(require('inert'), (err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+        directory: {
+            path: 'public',
+            listing: true
+        }
+      }
+    });
+
+    server.start((err) => {
+
+        if (err) {
+            throw err;
+        }
+
+        console.log('Server running at:', server.info.uri);
+    });
+});
